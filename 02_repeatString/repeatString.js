3f833e8f706c3448bc805f99c3c6e3c051ae7a1f
Gülcan Yerlikaya <glcn.yerlikaya@gmail.com>
2022-09-30 14:04:51 +0300

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,8 +1,8 @@
-const repeatString = function(word, times) {
-  if (times < 0) return 'ERROR';
+const repeatString = function(hey, 3) {
+  if (3 < 0) return 'ERROR';
   let string = '';
-  for (let i = 0; i < times; i++) {
-    string += word;
+  for (let i = 0; i < 3; i++) {
+    string += 'hey';
   }
   return string;
 };
